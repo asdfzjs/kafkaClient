4a78692372d7acd67ac87b5a40819b15d2f57fe8
zhujisong <zhujisong@SC-201605231355.zibang.com>
2016-10-13 10:38:44 +0800

    consumer

--- src/main/java/com/zillionfortune/consumer/kafkaConsumer.java
+++ src/main/java/com/zillionfortune/consumer/kafkaConsumer.java
@@ -26,5 +26,48 @@ public class kafkaConsumer {
 		     for (ConsumerRecord<String, String> record : records)
 		         System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
 		 }
+		 
+
+
+
+//手动控制偏移量
+//		 KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+//	     consumer.subscribe(Arrays.asList("foo", "bar"));
+//	     final int minBatchSize = 200;
+//	     List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
+//	     while (true) {
+//	         ConsumerRecords<String, String> records = consumer.poll(100);
+//	         for (ConsumerRecord<String, String> record : records) {
+//	             buffer.add(record);
+//	         }
+//	         if (buffer.size() >= minBatchSize) {
+//	             insertIntoDb(buffer);
+//	             consumer.commitSync();
+//	             buffer.clear();
+//	         }
+//	     }
+	
+//更精确的控制		 
+//		 try {
+//	         while(running) {
+//	             ConsumerRecords<String, String> records = consumer.poll(Long.MAX_VALUE);
+//	             for (TopicPartition partition : records.partitions()) {
+//	                 List<ConsumerRecord<String, String>> partitionRecords = records.records(partition);
+//	                 for (ConsumerRecord<String, String> record : partitionRecords) {
+//	                     System.out.println(record.offset() + ": " + record.value());
+//	                 }
+//	                 long lastOffset = partitionRecords.get(partitionRecords.size() - 1).offset();
+//	                 consumer.commitSync(Collections.singletonMap(partition, new OffsetAndMetadata(lastOffset + 1)));
+//	             }
+//	         }
+//	     } finally {
+//	       consumer.close();
+//	     }
+		 
+//订阅指定分区
+//		 String topic = "foo";
+//	     TopicPartition partition0 = new TopicPartition(topic, 0);
+//	     TopicPartition partition1 = new TopicPartition(topic, 1);
+//	     consumer.assign(Arrays.asList(partition0, partition1));
 	}
 }
